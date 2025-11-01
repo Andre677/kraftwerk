95968df2d37403c12fa4d5b61785cd9995fb5e9b
Andre677 <andamu-bis@online.de>
2025-11-01 13:43:47 +0100

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { registerSW } from "virtual:pwa-register";
-
-registerSW({
-  onNeedRefresh() {
-    // sagt der App: es gibt ein Update
-    window.dispatchEvent(new CustomEvent("kw_sw_update_available"));
-  },
-  onOfflineReady() {
-    // könnte Toast senden
-  },
-});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
